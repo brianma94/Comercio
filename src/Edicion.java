1b550f1bdfca97f1feec62bef6bb465888e95b8c
brianma94 <brian_1994_bcn@hotmail.com>
2017-03-13 19:44:39 +0100

    test

--- src/Edicion.java
+++ src/Edicion.java
@@ -14,7 +14,7 @@ public class Edicion extends javax.swing.JFrame {
      * Creates new form Edicion
      */
     public Edicion() {
-        initComponents();
+        initComponents(); 
     }
 
     /**
